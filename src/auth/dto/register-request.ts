79359c07b8ae8cc97ab9ace2e833e6e668952aab
Rizky Akbar Gusnaen <rizkyakbargusnaen@gmail.com>
2026-02-12 10:45:23 +0700

    chore: update reqister request schema

--- src/auth/dto/register-request.ts
+++ src/auth/dto/register-request.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 export const RegisterRequestSchema = z.object({
   email: z.email().min(1).max(255),
-  password: z.string().min(1).max(255),
+  password: z.string().min(8).max(255),
   name: z.string().min(1).max(255),
 });
 
